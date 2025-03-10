f467f75af8e28340f3e9a2360819b48d64e1eb94
Artur-FE <arturfedoseiev@gmail.com>
2025-03-10 14:09:32 +0100

    add inputs and currency rate output to input

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ let selectHeader = document.getElementById("selectHeader");
 let currencyForSaleSelected = 'appleee';
 let moneyForSale  = 0;
 let moneyForPurchase = 0;
+let moneyForPurchaseName = '';
+let moneyForSaleName = '';
+let inputCurrencyForPurchase = document.getElementById("inputCurrencyForPurchase");
+let inputCurrencyForSale = document.getElementById("inputCurrencyForSale");
 
 
 fetch(
@@ -34,16 +38,14 @@ fetch(
 //     });
 
 
-// получаем выбранную валюту в выпадающем списке для продажи
+// // получаем выбранную валюту в выпадающем списке для продажи
 
-const currencyForPurchase = document.getElementById('CurrencyForPurchase');
-const CurrencyForPurchaseValue = currencyForPurchase.options[currencyForPurchase.selectedIndex].value;
-console.log(CurrencyForPurchaseValue);
+// const currencyForPurchase = document.getElementById('CurrencyForPurchase');
+// const CurrencyForPurchaseValue = currencyForPurchase.options[currencyForPurchase.selectedIndex].value;
 
-//получаем выбранную валюту в выпадающем списке для покупки    
-let selectElement = document.getElementById('CurrencyForSale');
-    let selectedValue = selectElement.options[selectElement.selectedIndex].value;
-    console.log(selectedValue);
+// //получаем выбранную валюту в выпадающем списке для покупки    
+// let selectElement = document.getElementById('CurrencyForSale');
+//     let selectedValue = selectElement.options[selectElement.selectedIndex].value;
 
 
 
@@ -54,7 +56,7 @@ let selectElement = document.getElementById('CurrencyForSale');
     let currencyForPurchaseSelected = document.getElementById('CurrencyForPurchase').value;     
      
       
-      // выводим курс выбранной валюты относительно одного доллара
+      // выводим курс выбранных валют относительно друг-друга
         arrayKeys.forEach((element) => {
 //очищаем поле вывода от предыдущего значения 
           const currencyActualElement = document.getElementById('currencyActual');
@@ -65,11 +67,11 @@ let selectElement = document.getElementById('CurrencyForSale');
           
           if(element === currencyForSaleSelected) {
               
-              //выводим курс валюты которую хотим купить
-              moneyForSale  = `${latest.rates[element]} ${element}`;
-              console.log(moneyForSale);
-              
-                console.log(`1 USD = ${latest.rates[element] *0.9 + latest.rates[element]} ${element}`);
+              //записываем в переменные курс и название валюты которую хотим купить
+              moneyForSale  = `${latest.rates[element]}`;
+              moneyForSaleName = `${element}`;
+          
+           
                 // const li = document.createElement("li");
                 // li.id = "currencyActual"
                 // li.textContent = `
@@ -78,12 +80,12 @@ let selectElement = document.getElementById('CurrencyForSale');
                 // currency.appendChild(li);
             }
 
-            //выводим курс валюты которую хотим продать
+            //записываем в переменные курс и название валюты которую хотим продать
 
             if(element === currencyForPurchaseSelected) {
-             moneyForPurchase = `${latest.rates[element]} ${element}`;
-             console.log(moneyForPurchase);
-             
+             moneyForPurchase = `${latest.rates[element]}`;
+             moneyForPurchaseName = `${element}`;
+                       
             }
 
             // проверяем, что значения валют покупки продажи найдены и выводим результат на страницу
@@ -91,8 +93,12 @@ let selectElement = document.getElementById('CurrencyForSale');
               const li = document.createElement("li");
                 li.id = "currencyActual"
                 li.textContent = `
-                  ${moneyForPurchase} = ${moneyForSale}
+                  ${moneyForPurchase/moneyForPurchase} ${moneyForPurchaseName} = 
+                  ${moneyForSale/moneyForPurchase} ${moneyForSaleName}
                   `;
+               //выводим значения курсов в input на странице
+                  inputCurrencyForPurchase.value = moneyForPurchase/moneyForPurchase;
+                inputCurrencyForSale.value = moneyForSale/moneyForPurchase;
                 currency.appendChild(li);
                 return;
            }
@@ -101,11 +107,18 @@ let selectElement = document.getElementById('CurrencyForSale');
         
 };
 
-    // for (const key in latest.rates) {
+   // ловим значение (количество валюты) из input inputCurrencyForPurchase и выводим 
+   // в inputCurrencyForSale итоговую сумму которую получит человек при обмене количества валюты из inputCurrencyForPurchase
 
-    //       //  console.log(key, latest.rates[key]);
-    //         console.log(`1 USD = ${latest.rates[key]} - ${key}`)
-    // }
+    const inputCurrencyForSale = document.getElementById("inputCurrencyForSale");
+    const inputCurrencyForPurchase = document.getElementById("inputCurrencyForPurchase");
+  
+    inputCurrencyForPurchase.oninput = function() {
+      
+      console.log(inputCurrencyForSale.value);
+      
+      inputCurrencyForSale.value = inputCurrencyForPurchase.value * moneyForSale; 
+    };
   });
 
     
